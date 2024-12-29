3a525edda9876e56d4ddf121d4c0d088106a2eb5
Xiaoyu Huang <silwol@hotmail.com>
2024-12-29 21:46:27 +0800

    add network security group rules for http(s) and socks5 (#2)

--- local.tf
+++ local.tf
@@ -4,4 +4,20 @@ locals {
     "Operation-Tags.TerraformVersion" = var.terraform_version
     "Resource-Tags.Region"            = var.region
   }
+  security_rule_protocles = {
+    TCP    = "6"
+    UDP    = "17"
+    ICMP   = "1"
+    ICMPv6 = "58"
+    ALL    = "all"
+  }
+  security_rule_type = {
+    CIDR_BLOCK             = "CIDR_BLOCK"
+    SERVICE_CIDR_BLOCK     = "SERVICE_CIDR_BLOCK"
+    NETWORK_SECURITY_GROUP = "NETWORK_SECURITY_GROUP"
+  }
+  security_rule_direction = {
+    INGRESS = "INGRESS"
+    EGRESS  = "EGRESS"
+  }
 }
